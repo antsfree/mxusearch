fd5b8ba60fc0270b8bbb2f5f3227bc7ae89ebf8f
徐炜增 <xuweizeng@hoge.cn>
2017-08-10 09:33:55 +0800

    初始化仓库 添加扩展服务

---
+++ config/mxusearch.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'host'       => env('XUNSEARCH_HOST', '127.0.0.1'),
+    'searchport' => env('XUNSEARCH_SEARCH_PORT', '8384'),
+    'indexport'  => env('XUNSEARCH_INDEX_PORT', '8383'),
+];
